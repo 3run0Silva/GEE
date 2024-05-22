5131ce0d9130ad0f7951a4bd5daa2547071c6a49
Bruno Silva <138171223+3run0Silva@users.noreply.github.com>
2024-05-22 12:32:42 +0200

    Add: added port 8080
    Added port 8080 to host 0.0.0.0 to be able to deploy it

--- api/app.py
+++ api/app.py
@@ -19,4 +19,4 @@ app.register_blueprint(event_blueprint, url_prefix='/events')
 # app.register_blueprint(login_blueprint, url_prefix='/login')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=8080)
